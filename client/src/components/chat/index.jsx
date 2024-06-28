e06c901b235289af9b90061c33fa8d75add1e8c4
SravanamCharan20 <sravanamcharan20@gmail.com>
2024-06-28 16:15:42 +0530

    Chat Custom Header,Chat Custom Message Form Added

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
 import Header from "@/components/customHeader"
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 const Chat = () => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
@@ -14,7 +15,13 @@ const Chat = () => {
       <MultiChatWindow
         {...chatProps}
         style={{ height: "100vh" }}
-        renderChatHeader={(chat) => <Header chat={chat} />}/>
+        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) =>{
+          return (
+            <StandardMessageForm props={props} activeChat =  {chatProps.chat}/>  
+          )
+        }}
+        />
     </div>
   )
 }
